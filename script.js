439cedff1649597b2a9ee74bf420d6d810488643
Denff <manowardenff@gmail.com>
2021-06-11 13:02:49 +0500

    fix2

--- script.js
+++ script.js
@@ -1,10 +1,47 @@
+
+
+
 const graph01 = document.getElementById('myChart').getContext('2d');
 
-const blue_gray_gradient = graph01.createLinearGradient(0, 0, 0, 534);
+const blue_gray_gradient = graph01.createLinearGradient(0, 0, 0, 734);
 
 blue_gray_gradient.addColorStop(0, '#395B89');
-blue_gray_gradient.addColorStop(.7, '#F2F4F6');
+// blue_gray_gradient.addColorStop(.4, '#395B89');
+blue_gray_gradient.addColorStop(.2, '#395B89');
+// blue_gray_gradient.addColorStop(.2, '#F2F4F6');
 blue_gray_gradient.addColorStop(1, '#F2F4F6');
+
+
+var customTooltips = function (tooltip) {
+			$(this._chart.canvas).css("cursor", "pointer");
+
+			var positionY = this._chart.canvas.offsetTop;
+			var positionX = this._chart.canvas.offsetLeft;
+
+			$(".chartjs-tooltip").css({
+				opacity: 0,
+			});
+
+			if (!tooltip || !tooltip.opacity) {
+				return;
+			}
+
+			if (tooltip.dataPoints.length > 0) {
+				tooltip.dataPoints.forEach(function (dataPoint) {
+					var content = [dataPoint.xLabel, dataPoint.yLabel].join(": ");
+					var $tooltip = $("#tooltip-" + dataPoint.datasetIndex);
+
+					$tooltip.html(content);
+					$tooltip.css({
+						opacity: 1,
+						top: positionY + dataPoint.y + "px",
+						left: positionX + dataPoint.x + "px",
+					});
+				});
+			}
+		};
+
+
 
 const myChart = new Chart(graph01, {
         type: 'line',
@@ -18,30 +55,58 @@ const myChart = new Chart(graph01, {
             '520','534'
             ],
             datasets: [{
-                label: 'Filled',
+                label: 'Доход в %',
                 backgroundColor: blue_gray_gradient,
-                borderColor: 'rgb(75, 192, 192)',
+                borderColor: 'rgba(57, 91, 137, 0.3)',
                 data: [
                     4.5, 5, 4.5, 6, 5.5, 7.5, 12, 12, 15, 13.8,
                     14.1, 13.1, 22, 23.5, 22, 22.4, 22, 22, 23, 23.5,
                     26, 27, 23.5, 24.5, 23, 24.7, 24.8, 23.2, 26, 22,
                     20, 20, 24, 23.5, 25, 24.4, 24.6, 24, 26, 33.05
                 ],
+                borderWidth: 1,
                 fill: true,
             }]
         },
         options: {
+
             plugins: {
-            filler: {
-                propagate: true,
+                tooltips: {
+                    position: 'nearest',
+                    mode: 'index',
+                    intersect: false,
+                    // yPadding: 10,
+                    // xPadding: 10,
+                    // caretSize: 8,
+                    // backgroundColor: 'rgba(72, 241, 12, 1)',
+                    // titleFontColor: window.chartColors.black,
+                    // bodyFontColor: window.chartColors.black,
+                    // borderColor: 'rgba(200,200,200,1)',
+                    // borderWidth: 4,
+                    custom: customTooltips
+                },
+                legend: {
+                    labels: {
+                        font: {
+                            size: 18
+                        }
+                    }
+                },
+                filler: {
+                    propagate: true,
+                },
+                title: {
+                    display: false,
+                    text: (ctx) => 'Fill: ' + ctx.chart.data.datasets[0].fill
+                }
             },
-            title: {
-                display: true,
-                text: (ctx) => 'Fill: ' + ctx.chart.data.datasets[0].fill
-            }
+            pointStyle: 'circle',
+            radius: 1,
+            layout: {
+                padding: 70
             },
             interaction: {
-            intersect: false,
+                intersect: false,
             }
         }
     }
